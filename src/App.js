16c8d6f7ac8df5ede8b1356a5a17503f7f9f808b
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-11-22 17:01:49 -0500

    formulario de creacion de usuario listo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Nav from "./components/Nav/Nav";
+import RegisterUser from "./views/Register"
 
-function App() {
+function App()
+{
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Nav />} />
+        <Route path="/registerUser" element={<RegisterUser />} />
       </Routes>
     </BrowserRouter>
   );
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-import { getEjemploSlice } from "../reducers/getEjemploSlice";
+// import { getEjemploSlice } from "../reducers/getEjemploSlice";
 
-// get ejemplo
-export const getEjemAll = () => async (dispatch) => {
-  dispatch(getEjemploSlice());
-};
+const api = 'http://localhost:3001'
+
+// // get ejemplo
+// export const getEjemAll = () => async (dispatch) =>
+// {
+//   dispatch(getEjemploSlice());
+// };
+
+export const createNewUser = (data) => async () =>
+{
+  await axios({
+    method: 'POST',
+    url: `${api}/user/register`,
+    data: data
+  }).catch((e) => console.log(e));
+}
