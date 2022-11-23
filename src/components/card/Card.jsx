0bbeb07e1c138dea4a3fd516487ba9a99b66b5f9
Alex <alexballen53@gmail.com>
2022-11-22 22:08:08 -0500

    creacion de paginado, y estilos de las card renderizadas

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = ({ name, description, image, unitPrice }) => {
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl m-8">
+    <div className="card card-compact w-96 h-96 bg-base-100 shadow-xl m-8">
       <figure className="mt-5">
-        <img src={image} alt="Not found" width={100} height={70} />
+        <img src={image} alt="Not found" width={350} height={300} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
