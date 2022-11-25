3efbf3f780be24c322edf13c0a5028d721b8c663
Alex <alexballen53@gmail.com>
2022-11-25 12:48:29 -0500

    Paginado guarde el historial cuando regrese del detalle vuelva a la pagina que estaba actual

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getProducts } from "../redux/actions";
+import {
+  getProducts,
+  Clearproduct,
+  currentPagePaginated,
+} from "../redux/actions";
 import Card from "../components/card/Card";
 import FilterContainer from "../components/filter/FilterContainer";
 import Paginated from "../components/paginated/Paginated";
 import BestProducts from "../components/BestProducts/BestProducts";
-import { Clearproduct } from "../redux/actions";
-const Home = () =>
-{
+
+const Home = () => {
   const dispatch = useDispatch();
   const { filteredProducts: products } = useSelector((state) => state.products);
+  const { page } = useSelector((state) => state.page);
+  console.log(page);
 
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(page);
+  console.log(currentPage);
   const [productsByPage, setProductsByPage] = useState(8);
 
   const indexLastProduct = currentPage * productsByPage;
@@ -21,15 +27,16 @@ const Home = () =>
       ? products.slice(indexFirstProduct, indexLastProduct)
       : products.slice(indexFirstProduct, indexLastProduct);
 
-  const paginated = (numPage) =>
-  {
+  const paginated = (numPage) => {
+    dispatch(currentPagePaginated(numPage));
     setCurrentPage(numPage);
   };
 
-  useEffect(() =>
-  {
+  useEffect(() => {
     dispatch(Clearproduct());
     dispatch(getProducts());
+
+    //dispatch(currentPagePaginated());
   }, [dispatch]);
 
   return (
@@ -50,21 +57,20 @@ const Home = () =>
         </div>
         <div className="bg-base-300 flex flex-wrap justify-evenly items-start content-around ">
           {productCurrent.length > 0
-            ? productCurrent.map((e, i) =>
-            {
-              return (
-                <div key={i} className="w-400 h-250">
-                  <Card
-                    id={e.id}
-                    image={e.photos[0].url}
-                    brand={e.brand.name}
-                    name={e.name}
-                    unitPrice={e.unitPrice}
-                    description={e.description}
-                  />
-                </div>
-              );
-            })
+            ? productCurrent.map((e, i) => {
+                return (
+                  <div key={i} className="w-400 h-250">
+                    <Card
+                      id={e.id}
+                      image={e.photos[0].url}
+                      brand={e.brand.name}
+                      name={e.name}
+                      unitPrice={e.unitPrice}
+                      description={e.description}
+                    />
+                  </div>
+                );
+              })
             : "No Products"}
         </div>
         <div>
