80ec26e571c9b7c7c3b511d32ad427faa20f5870
Alex <alexballen53@gmail.com>
2022-12-04 21:52:19 -0500

    nav ok

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,16 +2,15 @@
 import React, { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { getCurrentUser } from "../../redux/actions/index"
+import { getCurrentUser } from "../../redux/actions/index";
 import { useSelector, useDispatch } from "react-redux";
 //import logo from "../../images/HCoutureLogo.png";
 
 import UserCart from "../Cart/UserCart";
 
 const Nav = () => {
-
   const { isAuthenticated, logout, loginWithPopup, user } = useAuth0();
-  const { loggedUser } = useSelector(state => state.user);
+  const { loggedUser } = useSelector((state) => state.user);
 
   // const navigate = useNavigate()
   // if(loggedUser.phoneNumber ===null || loggedUser.cityOfOrigin ===null){
@@ -20,7 +19,10 @@ const Nav = () => {
 
   return (
     <>
-      <div className="navbar flex justify-between bg-white" style={{ width: "100%" }}>
+      <div
+        className="navbar flex justify-between bg-white"
+        style={{ width: "100%" }}
+      >
         <div style={{ width: "fit-content" }}>
           <Link to={"/"} style={{ width: "fit-content" }}>
             <img
@@ -46,81 +48,75 @@ const Nav = () => {
             </span>
           )}
         </div>
-        {isAuthenticated && <UserCart />}
 
         {isAuthenticated && (
-          <div className=" mr-8 m-2">
-
+          <div className=" mr-8 mt-3">
             <UserCart />
 
-          {loggedUser && loggedUser.isAdmin && (
-            <div className=" flex-1">
-              <Link to="/dashboard">
-                <button className="btn btn-ghost normal-case   text-black  ml-8 text-base">
-                  Panel de Admin
-                </button>
-              </Link>
-            </div>
-          )}
-
-      
-
-          {loggedUser ? (
-            <div className="dropdown dropdown-end   ">
-              <label
-                tabIndex={0}
-                className="  btn btn-ghost btn-circle  avatar"
-              >
-                {loggedUser.photo ? (
-                  <div className="w-10 rounded-full">
-                    <img src={loggedUser.photo.url} alt="profilepicture" />
-                  </div>
-                ) : (
-                  <div className="w-10 rounded-full">
-                    <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUemgh2Unk-2K8MrXuSRmawDNdccYPxRcCCQ&usqp=CAU"
-                      alt="profilepicture"
-                    />
-                  </div>
-                )}
-                <span className="justify-between text-black">
-                  {user.name ? user.name : user.nickname}
-                </span>
-              </label>
-              {/* active mediante log in o inactivemediante log out */}
-              <ul className="menu    menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <Link to={`/user/${loggedUser.id}`}>
-                  <li>
-                    <span className="justify-between text-xl  ">Perfil</span>
-                  </li>
-                </Link>
-
-                <Link to={"/favorites"}>
-                  <li>
-                    <span className="justify-between text-xl  ">
-                      Mis favoritos
-                    </span>
-                  </li>
-                </Link>
-
-                <Link to={"/Historial"}>
-                  <li>
-                    <span className="justify-between text-xl  ">
-                      Mis compras
-                    </span>
-                  </li>
-                </Link>
-
-                <li>
-                  <button className="text-xl" onClick={logout}>
-                    Cerrar Sesión
+            {loggedUser && loggedUser.isAdmin && (
+              <div className=" flex-1">
+                <Link to="/dashboard">
+                  <button className="btn btn-ghost normal-case text-black text-base mr-8">
+                    Panel de Admin
                   </button>
-                </li>
-              </ul>
-            </div>
-          ) : (
-            null
-          )}
+                </Link>
+              </div>
+            )}
+
+            {loggedUser ? (
+              <div className="dropdown dropdown-end   ">
+                <label
+                  tabIndex={0}
+                  className="  btn btn-ghost btn-circle  avatar"
+                >
+                  {loggedUser.photo ? (
+                    <div className="w-10 rounded-full">
+                      <img src={loggedUser.photo.url} alt="profilepicture" />
+                    </div>
+                  ) : (
+                    <div className="w-10 rounded-full">
+                      <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUemgh2Unk-2K8MrXuSRmawDNdccYPxRcCCQ&usqp=CAU"
+                        alt="profilepicture"
+                      />
+                    </div>
+                  )}
+                  <span className="justify-between text-black">
+                    {user.name ? user.name : user.nickname}
+                  </span>
+                </label>
+                {/* active mediante log in o inactivemediante log out */}
+                <ul className="menu    menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  <Link to={`/user/${loggedUser.id}`}>
+                    <li>
+                      <span className="justify-between text-xl  ">Perfil</span>
+                    </li>
+                  </Link>
+
+                  <Link to={"/favorites"}>
+                    <li>
+                      <span className="justify-between text-xl  ">
+                        Mis favoritos
+                      </span>
+                    </li>
+                  </Link>
+
+                  <Link to={"/Historial"}>
+                    <li>
+                      <span className="justify-between text-xl  ">
+                        Mis compras
+                      </span>
+                    </li>
+                  </Link>
+
+                  <li>
+                    <button className="text-xl" onClick={logout}>
+                      Cerrar Sesión
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ) : null}
           </div>
         )}
       </div>
